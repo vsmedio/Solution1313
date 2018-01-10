bfa42e9cbaa5f815bcfd630f6cf8e9028646f51e
Arvis Abromovics <info@vsmedio.lv>
2018-01-10 19:02:15 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+
+/* 
+Лисица — это такое животное
+1. Реализуй в классе Fox интерфейс Animal.
+2. Поменяй код так, чтобы в классе Fox был только один метод - getName.
+3. Создавать дополнительные классы и удалять методы нельзя!
+
+
+Требования:
+1. Интерфейс Animal должен быть реализован в классе Fox.
+2. В классе Fox должен быть реализован только один метод(getName).
+3. В интерфейсе Animal должен быть объявлен метод getColor.
+4. Дополнительные классы или интерфейсы создавать нельзя.
+
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+    }
+
+    public static class Fox {
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+    }
+
+    public abstract static class Fox implements Animal {
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
